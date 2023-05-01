98d909ff05eb69d430ae40f448aa0d87e21171ca
Joe Krall <hello@joekrall.com>
2023-05-01 14:10:18 -0700

    ehh, got rid of packer bootstrap

--- lua/joe/packer.lua
+++ lua/joe/packer.lua
@@ -1,19 +1,5 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
--- bootstrapping on new machine
-local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
-end
-
-local packer_bootstrap = ensure_packer()
-
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
@@ -65,15 +51,5 @@ return require('packer').startup(function(use)
     }
 
     use 'glench/vim-jinja2-syntax' -- Nunjucks!!
-    use { -- git diffing
-        'sindrets/diffview.nvim',
-        requires = 'nvim-lua/plenary.nvim'
-    }
-
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
 end)
 
