295d0901239c5150d6b35d15ceb90a90dc56de39
Joe Krall <hello@joekrall.com>
2023-06-09 10:55:22 -0700

    (－‸ლ)

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -2,7 +2,7 @@ local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
 vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+vim.keymap.set("n", "<C-s>", ui.toggle_quick_menu)
 
 vim.keymap.set("n", "<C-n>", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<C-e>", function() ui.nav_file(2) end)
