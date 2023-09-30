de8cb1782b6005fb8167f64941846f994d08931a
Joe Krall <hello@joekrall.com>
2023-09-30 12:55:45 -0700

    remove codeium

--- lua/joe/packer.lua
+++ lua/joe/packer.lua
@@ -65,15 +65,6 @@ return require('packer').startup(function(use)
   }
 
   use 'glench/vim-jinja2-syntax'   -- Nunjucks!!
-  use {
-    'Exafunction/codeium.vim',
-    config = function()
-      vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true })
-      vim.keymap.set('i', '<C-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
-      vim.keymap.set('i', '<C-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
-      vim.keymap.set('i', '<C-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
-    end
-  }
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
