9faa02fb85251eafc50f163724cb5defe004998a
Joe Krall <hello@joekrall.com>
2023-10-30 12:14:46 -0700

    simplify, simplify

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -11,15 +11,7 @@ lsp.ensure_installed({
 })
 
 -- Fix Undefined global 'vim'
-lsp.configure('lua_ls', {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { 'vim' }
-            }
-        }
-    }
-})
+lsp.nvim_workspace()
 
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
