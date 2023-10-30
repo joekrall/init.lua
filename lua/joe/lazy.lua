2448095eaf764c220ad7c74959b8384e4aeb5a5b
Joe Krall <hello@joekrall.com>
2023-10-30 13:23:07 -0700

    telescope updates

--- lua/joe/lazy.lua
+++ lua/joe/lazy.lua
@@ -20,7 +20,7 @@ require("lazy").setup({
     -- or                            , branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim' },
   },
-
+  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   {
     'rose-pine/neovim',
     as = 'rose-pine',
