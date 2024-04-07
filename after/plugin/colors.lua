f0495b1f4fa54e48b35c9c1e93d4f9c6a1adbfb8
Joe Krall <hello@joekrall.com>
2024-04-06 20:47:16 -0700

    break out colors

---
+++ after/plugin/colors.lua
@@ -0,0 +1,8 @@
+require("rose-pine").setup({
+  styles = {
+    bold = false,
+    transparency = true,
+  },
+})
+
+vim.cmd('colorscheme rose-pine')
--- lua/joe/lazy.lua
+++ lua/joe/lazy.lua
@@ -24,15 +24,6 @@ require("lazy").setup({
   {
     'rose-pine/neovim',
     name = 'rose-pine',
-    config = function()
-      require("rose-pine").setup({
-        styles = {
-          bold = false,
-          transparency = true,
-        },
-      })
-      vim.cmd('colorscheme rose-pine')
-    end
   },
 
   {
