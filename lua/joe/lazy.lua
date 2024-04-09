a658b12bd0400ec71363965df4916c040067f730
Joe Krall <hello@joekrall.com>
2024-04-08 21:01:23 -0700

    Copilot

--- lua/joe/lazy.lua
+++ lua/joe/lazy.lua
@@ -50,19 +50,7 @@ require("lazy").setup({
     }
   },
   {
-    'Exafunction/codeium.vim',
-    event = 'BufEnter',
-    config = function()
-      vim.g.codeium_disable_bindings = 1
-      vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true })
-      vim.keymap.set('i', '<M-]>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
-      vim.keymap.set('i', '<M-[>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
-      vim.keymap.set('i', '<C-]>', function() return vim.fn['codeium#Clear']() end, { expr = true })
-      vim.keymap.set('i', '<M-Bslash>', function() return vim.fn['codeium#Complete']() end, { expr = true })
-      vim.g.codeium_filetypes = {
-        markdown = false,
-      }
-    end
+    "github/copilot.vim",
   },
   'glench/vim-jinja2-syntax' -- Nunjucks!!
 })
