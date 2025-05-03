202dc979544c62eed0d2babf3b07f13bd710058b
Joe Krall <hello@joekrall.com>
2025-05-03 18:05:40 -0400

    remove codeium

--- lua/joe/lazy.lua
+++ lua/joe/lazy.lua
@@ -57,20 +57,5 @@ require("lazy").setup({
     "stevearc/conform.nvim",
     event = { "BufReadPre", "BufNewFile" },
   },
-  {
-    'Exafunction/codeium.vim',
-    event = 'BufEnter',
-    config = function()
-      vim.g.codeium_disable_bindings = 1
-      vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true })
-      vim.keymap.set('i', '<M-]>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
-      vim.keymap.set('i', '<M-[>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
-      vim.keymap.set('i', '<C-]>', function() return vim.fn['codeium#Clear']() end, { expr = true })
-      vim.keymap.set('i', '<M-Bslash>', function() return vim.fn['codeium#Complete']() end, { expr = true })
-      vim.g.codeium_filetypes = {
-        markdown = false,
-      }
-    end
-  },
   'glench/vim-jinja2-syntax' -- Nunjucks!!
 })
