8cd4cc85026be1d543ef100c09c3acd3496a2713
Joe Krall <hello@joekrall.com>
2026-05-06 08:56:33 -0400

    fix typo

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -72,7 +72,7 @@ cmp.setup({
   snippet = { expand = function(args) require('luasnip').lsp_expand(args.body) end },
   window = {
     completion = cmp.config.window.bordered(),
-    documentaion = cmp.config.window.bordered()
+    documentation = cmp.config.window.bordered()
   }
 })
 
