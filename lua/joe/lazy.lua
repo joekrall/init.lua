cd99d0fcfa60112c3414cb9e1513d5231310ac3c
Joe Krall <hello@joekrall.com>
2026-05-06 08:56:49 -0400

    pin telescope to latest version

--- lua/joe/lazy.lua
+++ lua/joe/lazy.lua
@@ -16,8 +16,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.4',
-    -- or                            , branch = '0.1.x',
+    version = '*',
     dependencies = { 'nvim-lua/plenary.nvim' },
   },
   { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
