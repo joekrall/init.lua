90a481c27f1aa6524ed472cfbe44d0fedf062f45
Joe Krall <hello@joekrall.com>
2026-05-06 15:46:58 -0400

    reduce spacing

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -87,7 +87,7 @@ cmp.setup({
   formatting = {
     fields = { 'abbr', 'icon', 'kind', 'menu' },
     format = function(entry, vim_item)
-      vim_item.kind = string.format('%s  %s', kind_icons[vim_item.kind], vim_item.kind)
+      vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind)
       vim_item.menu = ({
         nvim_lsp = "[LSP]",
         luasnip = "[Snippet]",
