3b8199e3976e185f5e75108d06d253ee8308a523
Joe Krall <hello@joekrall.com>
2026-05-05 21:09:02 -0400

    updated tree-sitter config

--- lua/joe/lazy.lua
+++ lua/joe/lazy.lua
@@ -31,27 +31,16 @@ require("lazy").setup({
     branch = 'main',
     build = ':TSUpdate',
     config = function()
-      require('nvim-treesitter.configs').setup({
-        -- A list of parser names, or "all" (the listed parsers should always be installed)
-        ensure_installed = { "javascript", "typescript", "c", "lua", "vim", "vimdoc", "query", "gitcommit", "diff" },
+      require('nvim-treesitter').install { "javascript", "typescript", "c", "lua", "vim", "vimdoc", "query", "gitcommit", "diff" }
 
-        -- Install parsers synchronously (only applied to `ensure_installed`)
-        sync_install = false,
-
-        -- Automatically install missing parsers when entering buffer
-        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-        auto_install = true,
-
-        highlight = {
-          enable = true,
-
-
-          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-          -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-          -- Using this option may slow down your editor, and you may see some duplicate highlights.
-          -- Instead of true it can also be a list of languages
-          additional_vim_regex_highlighting = false,
-        },
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = '*',
+        callback = function(args)
+          local lang = vim.treesitter.language.get_lang(vim.bo[args.buf].filetype)
+          if lang then
+            pcall(vim.treesitter.start, args.buf, lang)
+          end
+        end,
       })
     end
 
